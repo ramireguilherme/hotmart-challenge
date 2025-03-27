cad78bf4c491df02a9d64969ac3ae0d003326cc9
Guilherme Ramirez <g217295@dac.unicamp.br>
2025-03-27 10:54:19 -0300

    Add Dockerfile and update app.py and requirements.txt

--- services/document_ingestion/app.py
+++ services/document_ingestion/app.py
@@ -1,3 +1,30 @@
 from fastapi import FastAPI
-
+import requests
+from utils import get_content_body, split_text
 app = FastAPI()
+
+@app.get("/scrape")
+def scrape_webpage(url: str):
+    response = requests.get(url)
+    if response.status_code == 200:
+        page_content = response.text
+        # save html content
+        with open("page.html", "w") as f:
+            f.write(page_content)
+    else:
+        print(f"Error processing page: {response.status_code}")
+
+    page_content = get_content_body(page_content)
+    content_chunks = split_text(page_content)
+    # store in vector db
+    
+
+    return {"content": page_content}
+
+
+@app.get("/question")
+def answer_question(question: str):
+    '''
+    Queries the rag module and returns the answer to the question.
+    '''
+    return {"question": question, "answer": "answer"}
